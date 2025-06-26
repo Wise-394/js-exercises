7f87ee8fbb518f6f94abcdf525d1ec13c5536b10
jr <114738222+Wise-394@users.noreply.github.com>
2025-06-26 11:30:51 +0800

    completed 07

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+//pseudo
+//a function reverse string that takes str as parameter
+//split the string via spaces and put it into array
+//reverse the array using the reverse method
+//join them
+const reverseString = function(str) {
+    return str.split("").reverse().join("")
 };
 
 // Do not edit below this line
