fd5ec098966b859e7b3a9bf2c4f7cc833620b315
jr <114738222+Wise-394@users.noreply.github.com>
2025-06-26 14:07:30 +0800

    completed 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+//implement a function that takes 2 positive integers 
+//and returns the sum of every integer between (and including) them:
 
+//make a sum variable
+//loop start with the firstNum
+//add the current num to the sum variable
+//end with secondNum
+const sumAll = function(firstNum, secondNum) {
+    if(!(Number.isInteger(firstNum) && Number.isInteger(secondNum))){
+        return "ERROR"
+    }
+    if(firstNum < 0 || secondNum < 0){
+        return "ERROR"
+    }
+
+    let sum = 0
+    const start = firstNum < secondNum ? firstNum : secondNum
+    const end = firstNum < secondNum ? secondNum : firstNum
+    for(let i = start; i <= end; i++){
+        sum += i
+    }
+    return sum
 };
 
 // Do not edit below this line
